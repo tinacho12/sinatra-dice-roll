57c31d7b8c29fb94ba4bf27e4bcb015463397890
tinacho12 <146214538+tinacho12@users.noreply.github.com>
2023-10-16 21:27:09 +0000

    Added 2d10

--- dice.rb
+++ dice.rb
@@ -25,3 +25,15 @@ get("/dice/2/6") do
   "<h1>2d6</h1>
    <p>#{outcome}</p>"
 end
+
+get("/dice/2/10") do
+  first_die = rand(1..10)
+  second_die = rand(1..10)
+  sum = first_die + second_die
+
+  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+
+  "<h1>2d10</h1>
+   <p>#{outcome}</p>"
+end
+
