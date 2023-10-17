d0e1a7fb455db0d7bd8a568307a44c87f33fe109
tinacho12 <146214538+tinacho12@users.noreply.github.com>
2023-10-17 19:16:02 +0000

    Added more gems, error message, hyperlinks to home

--- dice.rb
+++ dice.rb
@@ -2,9 +2,16 @@
 
 require "sinatra"
 require "sinatra/reloader"
+require "better_errors"
+require "binding_of_caller"
 
 get("/") do
-  "Hello World"
+  "<h1>Dice Roll</h1>
+  <ul>
+    <li><a href=\"/dice/2/6\">Roll two 6-sided dice</a></li>
+    <li><a href=\"/dice/2/10\">Roll two 10-sided dice</a></li>
+    <li><a href=\"/dice/1/20\">Roll one 20-sided die</a></li>
+    <li><a href=\"/dice/2/10\">Roll five 4-sided dice</a></li>"
 end
 
 get("/zebra") do
@@ -37,3 +44,29 @@ get("/dice/2/10") do
    <p>#{outcome}</p>"
 end
 
+get("/dice/1/20") do
+  first_die = rand(1..10)
+  second_die = rand(1..10)
+  sum = first_die + second_die
+
+  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+
+  "<h1>2d10</h1>
+   <p>#{outcome}</p>"
+end
+
+get("/dice/5/4") do
+  first_die = rand(1..10)
+  second_die = rand(1..10)
+  sum = first_die + second_die
+
+  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+
+  "<h1>2d10</h1>
+   <p>#{outcome}</p>"
+end
+
+# Need this configuration for better_errors
+use(BetterErrors::Middleware)
+BetterErrors.application_root = __dir__
+BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
