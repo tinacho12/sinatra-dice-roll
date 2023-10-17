b5f4265cc7896507cd14738076df1c2826e1a5e1
tinacho12 <146214538+tinacho12@users.noreply.github.com>
2023-10-17 19:20:16 +0000

    Played around with bug

--- dice.rb
+++ dice.rb
@@ -11,7 +11,7 @@ get("/") do
     <li><a href=\"/dice/2/6\">Roll two 6-sided dice</a></li>
     <li><a href=\"/dice/2/10\">Roll two 10-sided dice</a></li>
     <li><a href=\"/dice/1/20\">Roll one 20-sided die</a></li>
-    <li><a href=\"/dice/2/10\">Roll five 4-sided dice</a></li>"
+    <li><a href=\"/dice/5/4\">Roll five 4-sided dice</a></li>"
 end
 
 get("/zebra") do
